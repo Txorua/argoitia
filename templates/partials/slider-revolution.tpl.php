82d1e45f29fbcbee2ec47317a9b36afa8a42e204
Iñaki García Blasco <txorua@txorua.com>
2014-09-30 22:25:18 +0200

    Nuevas slides

--- templates/partials/slider-revolution.tpl.php
+++ templates/partials/slider-revolution.tpl.php
@@ -6,7 +6,7 @@
 		<li data-transition="fade"
               data-slotamount="10"
               data-masterspeed="300">
-              <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/slide-bg-0.jpg" class="img-responsive"  data-fullwidthcentering="on" alt="">
+              <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-0.jpg" data-fullwidthcentering="on" alt="">
 			  
 			                                                                                          
               
@@ -16,7 +16,7 @@
 	  <li data-transition="fade"
               data-slotamount="10"
               data-masterspeed="300">
-              <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/slide-bg-2.jpg" data-fullwidthcentering="on" alt="">
+              <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-1.jpg" data-fullwidthcentering="on" alt="">
 			  
 			                                                                                          
               
@@ -25,7 +25,7 @@
 	    <li data-transition="fade"
                 data-slotamount="10"
                 data-masterspeed="300">
-		  <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/slide-bg-3.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
+		  <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-2.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
 
 		
                 
@@ -35,7 +35,7 @@
 	    <li data-transition="fade"
                 data-slotamount="10"
                 data-masterspeed="300">
-                  <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/slide-bg-1.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
+                  <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-3.jpg" width="100%"; data-fullwidthcentering="on" alt="" class="defaultimg">
 
 		
 
@@ -44,13 +44,13 @@
 	   <li data-transition="fade"
 	       data-slotamount="10"
                data-masterspeed="300">
-                 <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/slide-bg-4.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
+                 <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-4.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
 
 
            </li>
 
            <li data-transition='fade' data-slotamount='10' data-masterspeed='300'>
-             <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/slide-bg-5.jpg" data-fullwidthcentering='on' alt=''>
+             <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-5.jpg" data-fullwidthcentering='on' alt=''>
                                        
              <div class='caption fade'
                   data-x='200'
@@ -66,7 +66,7 @@
 	   <li data-transition="fade"
                data-slotamount="10"
                data-masterspeed="300">
-		 <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/slide-bg-6.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
+		 <img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-6.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
 
 	     
                                              
@@ -77,7 +77,7 @@
 	  <li data-transition="fade"
               data-slotamount="10"
 	      data-masterspeed="300">
-		<img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/slide-bg-7.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
+		<img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-7.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
 
 	      <div class="caption fade tp-caption start"
                    data-x="100"
@@ -93,7 +93,7 @@
 			<li data-transition="fade"
               data-slotamount="10"
 	      data-masterspeed="300">
-		<img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/slide-bg-8.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
+		<img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-8.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
 
 	      <div class="caption fade tp-caption start"
                    data-x="100"
@@ -110,7 +110,24 @@
 				<li data-transition="fade"
               data-slotamount="10"
 	      data-masterspeed="300">
-		<img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/slide-bg-9.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
+		<img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-9.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
+
+	      <div class="caption fade tp-caption start"
+                   data-x="100"
+                   data-y="165"
+                   data-speed="250"
+                   data-start="5000"
+                   data-easing="easeOutExpo">
+                   <a href="#"></a></div>
+                        
+	     				
+            </li>
+			
+			
+			<li data-transition="fade"
+              data-slotamount="10"
+	      data-masterspeed="300">
+		<img src="<?php print base_path() . drupal_get_path('theme', 'argoitia'); ?>/images/slider-revolution/getariaturismo-slide-bg-10.jpg" data-fullwidthcentering="on" alt="" class="defaultimg">
 
 	      <div class="caption fade tp-caption start"
                    data-x="100"
